eac4280b7597768dbf5149a53208c40c509bd304
howesdomo <164572429@qq.com>
2018-06-20 10:58:44 +0800

    LinqToString.cs 组合集合的字符串并用指定的符号分隔他们

---
+++ Util/Linq/LinqToString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Linq
+{
+
+    public static class LinqToString
+    {
+        public static string CombineString<T>(this IEnumerable<T> list, string symbol = "; ")
+        {
+            string r = string.Empty;
+
+            foreach (var item in list)
+            {
+                if (r.IsNullOrWhiteSpace() == false)
+                {
+                    r = "{0}{1}{2}".FormatWith(r, symbol, item.ToString());
+                }
+                else
+                {
+                    r = item.ToString();
+                }
+            }
+
+            return r;
+            
+        }
+    }
+}
