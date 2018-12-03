058796a7d8b99403d1affbf074efd0f9a0a4a110
howesdomo <164572429@qq.com>
2018-12-03 10:34:50 +0800

    1 BusinessException 代码整理 2 Util/IO/SerialPort.cs  String 转换枚举类型 3 GZipUtils 压缩字符串, 对于JSON 等大量属性描述的数据有很大的压缩作用

--- Util/Exception/BusinessException.cs
+++ Util/Exception/BusinessException.cs
@@ -7,12 +7,17 @@ namespace System
 {
     public class BusinessException : Exception
     {
-        public BusinessException(string exMsg) : base(exMsg)
+        public const string cStart = "\u0002";
+
+        public const string cEnd = "\u0003";
+
+
+        public BusinessException(string exMsg) : base("{0}{1}{2}".FormatWith(cStart, exMsg, cEnd))
         {
 
         }
 
-        public BusinessException(string exMsg, Exception innerEx) : base(exMsg, innerEx)
+        public BusinessException(string exMsg, Exception innerEx) : base("{0}{1}{2}".FormatWith(cStart, exMsg, cEnd), innerEx)
         {
 
         }
