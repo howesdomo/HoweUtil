27855adf3dae813a5fe8283f75a20846fd9d7bac
howesdomo <164572429@qq.com>
2019-05-07 17:36:35 +0800

    将 ExceptionUtil.cs Exception.cs 整合到 ExceptionExtension.cs

--- Util/Exception/ExceptionExtension.cs
+++ Util/Exception/ExceptionExtension.cs
@@ -5,9 +5,6 @@ using System.Text;
 
 namespace System
 {
-    /// <summary>
-    /// V 1.0.0.1 加上 GetInfo, 用于简化 UI 报错的代码
-    /// </summary>
     public static class ExceptionExtension
     {
         /// <summary>
@@ -15,8 +12,6 @@ namespace System
         /// 
         /// 若是 返回 BusinessException 信息
         /// 否则 返回 GetFullInfo 信息
-        /// 
-        /// WPF 调用 WebService 时使用居多
         /// </summary>
         /// <param name="e"></param>
         /// <returns></returns>
@@ -34,11 +29,10 @@ namespace System
                 return msg;
             }
         }
-
         public static string GetFullInfo(this Exception e)
         {
             StringBuilder sb = new StringBuilder();
-            Util.ExceptionUtil.GetExceptionFullInfo(e, sb);
+            ExceptionExtension.GetExceptionFullInfo(e, sb);
             return sb.ToString();
         }
 
@@ -51,23 +45,65 @@ namespace System
         public static string GetFullInfoForInvoke(this Exception e)
         {
             StringBuilder sb = new StringBuilder();
-            Util.ExceptionUtil.GetExceptionFullInfoForInvoke(e, sb);
+            ExceptionExtension.GetExceptionFullInfoForInvoke(e, sb);
             return sb.ToString();
         }
 
+        public static string GetExceptionInfo(Exception ex)
+        {
+            return ex.Message + "\r\n" + ex.StackTrace;
+        }
+
+        public static void GetExceptionFullInfo(Exception ex, StringBuilder sb, int level = 1)
+        {
+            if (string.IsNullOrEmpty(sb.ToString()) == false)
+            {
+                sb.AppendLine("************** Inner Exception " + level + "**************");
+            }
+
+            sb.AppendLine(ex.Message + "\r\n" + ex.StackTrace);
+
+            if (ex.InnerException != null)
+            {
+                level = level + 1;
+                GetExceptionFullInfo(ex.InnerException, sb, level);
+            }
+        }
+
+        /// <summary>
+        /// 反射方式调用WebService, 去掉 第一层错误 调用的目标发生了异常。
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <param name="sb"></param>
+        /// <param name="level"></param>
+        public static void GetExceptionFullInfoForInvoke(Exception ex, StringBuilder sb, int level = 1)
+        {
+            if (ex.Message.Equals("调用的目标发生了异常。") == false) // 跳过调用目标发生异常
+            {
+                sb.AppendLine(ex.Message + "\r\n" + ex.StackTrace);
+                level = level + 1;
+            }
+
+            if (ex.InnerException != null)
+            {
+                GetExceptionFullInfo(ex.InnerException, sb, level);
+            }
+        }
+
+        /// <summary>
+        /// 判断是否为 BusinessException
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
         public static bool IsBusinessException(this Exception e)
         {
             string msg = e.GetFullInfo();
-            
+
             int indexOf_cStart = msg.IndexOf(BusinessException.cStart);
             int indexOf_cEnd = msg.IndexOf(BusinessException.cEnd);
 
             bool r = indexOf_cStart >= 0 && indexOf_cEnd > indexOf_cStart;
             return r;
         }
-
-
-
-
     }
 }
