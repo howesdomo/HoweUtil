f96ad91110a350bffd35b9cf13060a94ce289dc0
howesdomo <164572429@qq.com>
2020-01-03 17:26:25 +0800

    EventArgs{T}补充注释

--- Util.HowesDOMO/EventArgs/EventArgs{T}.cs
+++ Util.HowesDOMO/EventArgs/EventArgs{T}.cs
@@ -1,7 +1,8 @@
 ﻿namespace System
 {
     /// <summary>
-    /// Generic event argument class
+    /// V 1.0.0 - 2020-1-3 17:08:37
+    /// 首次创建 - 新引入 EventArgs<T>, 懒人的福音, 不用再去创建EventArgs的新的继承类
     /// </summary>
     /// <typeparam name="T">Type of the argument</typeparam>
     public class EventArgs<T> : EventArgs
--- Util.HowesDOMO/EventArgs/EventExtensions.cs
+++ Util.HowesDOMO/EventArgs/EventExtensions.cs
@@ -1,7 +1,8 @@
 ﻿namespace System
 {
     /// <summary>
-    /// Event extensions.
+    /// V 1.0.0 - 2020-1-3 17:08:37
+    /// 首次创建
     /// </summary>
     public static class EventExtensions
     {
