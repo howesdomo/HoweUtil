9f3640d02db694253a470782d10c558803d7ff35
howesdomo <164572429@qq.com>
2020-07-31 10:19:03 +0800

    V 1.0.10.0 整理 Font 相关代码 优化 FontFamilyUtils

--- Util.HowesDOMO/Font/FontFamilyUtils.cs
+++ Util.HowesDOMO/Font/FontFamilyUtils.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Util
+namespace Util_Font
 {
 	/// <summary>
+	/// V 1.0.1 - 2020-07-31 10:13:04
+	/// 1 修改 namespace
+	/// 2 弃用 GetName 方法
+	/// 3 优化 GetFontFamily_TypeOf_System_Drawing 的 fontName 
+	/// 
 	/// V 1.0.0 - 2020-03-13 13:38:14
 	/// 首次创建 方便获取指定字体
 	/// </summary>
 	public class FontFamilyUtils
 	{
+		[Obsolete] // 1.0.1 Winform 与 WPF 不再共同使用本方法进行获取字体名称
 		public static string GetFontName(string fontFilePath)
 		{
 			var glyphTypeface = new System.Windows.Media.GlyphTypeface(new Uri(fontFilePath));
@@ -19,7 +25,8 @@ namespace Util
 
 		public static System.Windows.Media.FontFamily GetFontFamily_TypeOf_System_Windows_Media(string fontFilePath)
 		{
-			string fontName = GetFontName(fontFilePath);
+			var glyphTypeface = new System.Windows.Media.GlyphTypeface(new Uri(fontFilePath));
+			string fontName = glyphTypeface.FamilyNames.Values.FirstOrDefault(); // 获取字体名
 
 			var directory = System.IO.Path.GetDirectoryName(fontFilePath);
 			var fontUri = $"{new Uri(directory).AbsoluteUri}/#{fontName}";
@@ -31,7 +38,12 @@ namespace Util
 		{
 			var collection = new System.Drawing.Text.PrivateFontCollection();
 			collection.AddFontFile(fontFilePath);
-			string fontName = GetFontName(fontFilePath);
+
+			string fontName = collection.Families[0].Name;
+
+			//// (供参考)检测字体类型是否可用
+			//var regular = collection.Families[0].IsStyleAvailable(System.Drawing.FontStyle.Regular);
+			//var bold = collection.Families[0].IsStyleAvailable(System.Drawing.FontStyle.Bold);
 
 			return new System.Drawing.FontFamily(fontName, collection);
 		}
