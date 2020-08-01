bce3ee73f75db9488e34f8ef1a49850755ffbc06
howesdomo <164572429@qq.com>
2020-08-01 22:06:17 +0800

    Util.HowesDOMO - 1.0.11 1 增加 IListExtension 2 增加 WebDateTime

---
+++ Util.HowesDOMO/CommonExtensions/IListExtension.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Collections
+{
+    /// <summary>
+    /// V 1.0.0 - 2020-07-21 15:41:22
+    /// 首次创建
+    /// </summary>
+    public static class IListExtension
+    {
+        public static IList Except(this IList bigger, IList smaller)
+        {
+            return IListExtension.Except_ActualMethod(bigger, smaller);
+        }
+
+        static IList Except_ActualMethod(IList bigger, IList smaller)
+        {
+            List<object> l = new List<object>();
+
+            if (smaller == null)
+            {
+                return bigger;
+            }
+
+            for (int indexOfBigger = 0; indexOfBigger < bigger.Count; indexOfBigger++)
+            {
+                bool isMatch = false;
+
+                int bHashCode = bigger[indexOfBigger].GetHashCode();
+                for (int indexOfSmaller = 0; indexOfSmaller < smaller.Count; indexOfSmaller++)
+                {
+                    int sHashCode = smaller[indexOfSmaller].GetHashCode();
+                    if (bHashCode == sHashCode)
+                    {
+                        isMatch = true;
+                        break;
+                    }
+                }
+
+                if (isMatch == false)
+                {
+                    l.Add(bigger[indexOfBigger]);
+                }
+            }
+
+            if (l.Count <= 0)
+            {
+                return null;
+            }
+
+            IList r = (IList)l;
+            return r;
+        }
+    }
+}
---
+++ Util.HowesDOMO/WebDateTime.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+
+namespace System
+{
+    public class WebDateTime
+    {
+        private WebDateTime()
+        {
+
+        }
+
+        public static DateTime Now
+        {
+            get
+            {
+                DateTime r = DateTime.Now;
+
+                WebRequest request = null;
+                WebResponse response = null;
+                WebHeaderCollection headerCollection = null;
+                string datetime = string.Empty;
+                try
+                {
+                    request = WebRequest.Create("https://www.baidu.com");
+                    request.Timeout = 3000;
+                    request.Credentials = CredentialCache.DefaultCredentials;
+                    response = (WebResponse)request.GetResponse();
+                    headerCollection = response.Headers;
+                    foreach (var h in headerCollection.AllKeys)
+                    {
+                        if (h == "Date")
+                        {
+                            datetime = headerCollection[h];
+                        }
+                    }
+                }
+                catch (Exception ex1)
+                {
+                    throw new Exception("GetInternetDateTime Step 1 Error", ex1);
+                }
+                finally
+                {
+                    if (request != null)
+                    {
+                        request.Abort();
+                    }
+
+                    if (response != null)
+                    {
+                        response.Close();
+                    }
+
+                    if (headerCollection != null)
+                    {
+                        headerCollection.Clear();
+                    }
+                }
+
+                try
+                {
+                    r = Convert.ToDateTime(datetime);
+                }
+                catch (Exception ex2)
+                {
+                    throw new Exception("GetInternetDateTime Step 1 Error", ex2);
+                }
+
+                return r;
+            }
+        }
+
+    }
+}
