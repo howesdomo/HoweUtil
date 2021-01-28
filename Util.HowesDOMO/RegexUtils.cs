973fce8cf669c5186dfeb161b4606d3f1af41624
HowesDOMO <164572429@qq.com>
2021-01-28 17:56:31 +0800

    Util.HowesDOMO V 1.0.17 1 增加用于整理正则表达式的工具类 RegexUtils.cs 2 增加 ObservableQueue.cs , 便于UI绑定, 并含有属性 QueueMaxCapacity(队列最大容量), 可以自动根据最大容量去掉多出来的元素

---
+++ Util.HowesDOMO/RegexUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Util
+{
+    /// <summary>
+    /// V 1 - 2021-01-25 17:13:00
+    /// 常用正则表达式收集
+    /// </summary>
+    public class RegexUtils
+    {
+        /// <summary>
+        /// IP地址
+        /// </summary>
+        public static string IPAddress = @"^(\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])$";
+
+
+        /// <summary>
+        /// 端口
+        /// </summary>
+        public static string Port = @"^([0-9]|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])$";
+    }
+}
---
+++ Util.HowesDOMO/UIComponent/ObservableQueue.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace Util.UIComponent
+{
+    /// <summary>
+    /// V 1.0.0 - 2021-01-28 17:00:00
+    /// 首次创建
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ObservableQueue<T> : Queue<T>, INotifyCollectionChanged
+    {
+        private int? _QueueMaxCapacity;
+        /// <summary>
+        /// 队列最大容量 ( 默认为空, 不设定最大容量 )
+        /// </summary>
+        public int? QueueMaxCapacity
+        {
+            get { return _QueueMaxCapacity; }
+            set
+            {
+                _QueueMaxCapacity = value;
+                fix();
+            }
+        }
+
+        public ObservableQueue(int? maxCapacity = null)
+        {
+            QueueMaxCapacity = maxCapacity;
+        }
+
+        public ObservableQueue(int capacity, int? maxCapacity = null) : base(capacity)
+        {
+            QueueMaxCapacity = maxCapacity;
+        }
+
+        public ObservableQueue(IEnumerable<T> collection, int? maxCapacity = null) : base(collection)
+        {
+            QueueMaxCapacity = maxCapacity;
+        }
+
+        public event System.Collections.Specialized.NotifyCollectionChangedEventHandler CollectionChanged;
+
+        public new void Clear()
+        {
+            base.Clear();
+            if (this.CollectionChanged != null)
+            {
+                CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            }
+        }
+
+        public new void Enqueue(T item)
+        {
+            base.Enqueue(item);
+            fix();
+            if (this.CollectionChanged != null)
+            {
+                CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            }
+        }
+
+        public new T Dequeue()
+        {
+            T item = base.Dequeue();
+            fix();
+            if (this.CollectionChanged != null)
+            {
+                CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            }
+            return item;
+        }
+
+        /// <summary>
+        /// 根据队列最大容量, 重新计算队列内的元素
+        /// </summary>
+        void fix()
+        {
+            if (QueueMaxCapacity.HasValue == false)
+            {
+                return;
+            }
+
+            // 被移出队列的数量 = 当前数量 - 最大容量
+            int toDequeueCount = this.Count - QueueMaxCapacity.Value;
+            if (toDequeueCount <= 0)
+            {
+                return;
+            }
+
+            for (int i = 0; i < toDequeueCount; i++)
+            {
+                base.Dequeue();
+            }
+
+            CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+    }
+}
